4177701fb79bd3297a2d388839b57a562cd5f80f
Alaa Deen Albarghoth <Eng.alaa.brghoth@gmail.com`>
2019-06-12 16:35:51 +0300

    Migrate the app to Androidx

--- app/src/main/java/com/alaaeddin/navcontroller/ui/main/MainFragment.kt
+++ app/src/main/java/com/alaaeddin/navcontroller/ui/main/MainFragment.kt
@@ -1,14 +1,14 @@
 package com.alaaeddin.navcontroller.ui.main
 
-import android.arch.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.alaaeddin.navcontroller.R
 
-class MainFragment : Fragment() {
+class MainFragment : androidx.fragment.app.Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
@@ -28,5 +28,4 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }
